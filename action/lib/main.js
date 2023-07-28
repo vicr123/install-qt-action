9d0805bff61336547abcd2f9c0f6d1048a684a6c
Victor Tran <vicr12345@gmail.com>
2023-07-28 20:23:16 +1000

    Fix regex

--- action/lib/main.js
+++ action/lib/main.js
@@ -66,7 +66,7 @@ const locateQtArchDir = (installDir) => {
     // For Qt6 mobile and wasm installations, and Qt6 Windows on ARM installations,
     // a standard desktop Qt installation must exist alongside the requested architecture.
     // In these cases, we must select the first item that ends with 'android*', 'ios', 'wasm*' or 'msvc*_arm64'.
-    const requiresParallelDesktop = qtArchDirs.filter((p) => p.match(/6\.\d+\.\d+\/(android[^/]*|ios|wasm[^/]*|msvc[^/]_arm64)$/));
+    const requiresParallelDesktop = qtArchDirs.filter((p) => p.match(/6\.\d+\.\d+\/(android[^/]*|ios|wasm[^/]*|msvc[^/]*_arm64)$/));
     if (requiresParallelDesktop.length) {
         // NOTE: if multiple mobile/wasm installations coexist, this may not select the desired directory
         return requiresParallelDesktop[0];
